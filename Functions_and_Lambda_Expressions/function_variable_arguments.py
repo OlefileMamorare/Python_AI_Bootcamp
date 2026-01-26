f74e8dcd5397eb941528b3a9a31c40b494db5e73
olefile <mamorareolefile@gmail.com>
2026-01-26 21:06:00 +0200

    Learning *args and **kwargs

--- Functions_and_Lambda_Expressions/function_variable_arguments.py
+++ Functions_and_Lambda_Expressions/function_variable_arguments.py
@@ -1,1 +1,16 @@
 #Function Variable-Length Arguments: *args, **kwargs
+
+#*args example: allows you to input multiple arguments as a tuple
+def calculate_total_cost(base_cost, *args):
+    print(args)
+    return base_cost + sum(args)
+
+print(calculate_total_cost(10 , 5, 2, 6, 7))
+
+
+
+# **kwargs: allows a function to accept any number of keyword arguments
+# (arguments passed as key=value)
+def display_user_info(**user_data):
+    for key, value in user_data.items():
+        print(f'{key} => {value}')
