3813252e65da9e5d71139b0831f68f4420d8069f
olefile <mamorareolefile@gmail.com>
2026-02-02 21:01:47 +0200

    Learn tell() and seek() functions

--- Files_Directories/reading_file.py
+++ Files_Directories/reading_file.py
@@ -4,3 +4,14 @@ f = open("Files_Directories/config.txt", 'r')
 
 content = f.read(4)
 print(content)
+
+print(f.tell())# tell() method tells you which position the cursor is at
+
+#seek() method: used to move the cursor to a specific location
+#seek(0) will move the cursor to the beginning of the file
+f.seek(2)
+content = f.read(3)
+print(content)
+
+print(f.tell())
+f.close()
