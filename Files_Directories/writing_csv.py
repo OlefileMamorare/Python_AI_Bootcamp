7a2842a28384072d8f206032be91717c5efe308a
olefile <mamorareolefile@gmail.com>
2026-02-03 20:28:44 +0200

    Write to CSV files

---
+++ Files_Directories/writing_csv.py
@@ -0,0 +1,15 @@
+# Writing CSV files in python
+
+import csv
+
+with open('Files_Directories/model_performance.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Model Version', 'Accuracy', 'Response Time (ms)', 'Tokens'])
+
+    models = [
+        ('GPT-4', 98.7, 45, 300),
+        ('GPT-4o', 99.7, 42, 350)
+    ]
+
+    for model in models:
+        writer.writerow(model)
