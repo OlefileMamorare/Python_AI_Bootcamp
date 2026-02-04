0bce27a0d93181cd0a495ca105e11eea5591028e
olefile <mamorareolefile@gmail.com>
2026-02-04 20:28:17 +0200

    Write to CSV files

--- Files_Directories/Making_A_Request_To_OpenAI/openai_json.py
+++ Files_Directories/Making_A_Request_To_OpenAI/openai_json.py
@@ -1,6 +1,6 @@
 # Making an API request to OpenAI
 
-import requests
-import json
+#import requests
+#import json
 
-api_key = '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'
+#api_key = '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'
