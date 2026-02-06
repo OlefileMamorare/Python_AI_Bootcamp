b0707a454f095474f47f83ad8697a12b9d27adc0
olefile <mamorareolefile@gmail.com>
2026-02-06 21:11:33 +0200

    Understand regex

--- regular_exp_intro.py
+++ regular_exp_intro.py
@@ -1,3 +1,21 @@
 # Introduction to Regular Expressions (Regex)
 
 import re
+
+# \d : matches any digit (0-9)
+# \w : matches any alphanumeric character, including underscore (a-z, A-Z, 0-9, _)
+# \s : matches any whitespace character (space, tab, newline)
+# . (dot): matches any character except a newline
+
+
+
+# Quantifiers:
+# + matches one or more occurrences
+# * matches zero or more occurrences
+# ? matches zero or one occurrences
+# {n} matches exactly n occurrences
+# {n, m} matches between n and m occurrences
+
+post = 'Exploring the future with #ArtificialIntelligence and #MachineLearning! #AI #GenAI'
+hashtags = re.findall(r'#\w+', post)
+print(hashtags)
