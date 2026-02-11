40f2aa23680ebf16fc8cbe764b3a0b095aef22d1
olefile <mamorareolefile@gmail.com>
2026-02-11 16:59:16 +0200

    OOP Turtle Demo

---
+++ OOP_Fundamentals/oop_turtle.py
@@ -0,0 +1,13 @@
+# OOP Demo: The Turtle
+
+from turtle import Turtle, Screen
+
+my_screen = Screen()
+donatello = Turtle()
+
+donatello.shape('turtle')
+donatello.color('purple')
+
+
+
+my_screen.exitonclick()
