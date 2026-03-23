98cf792f124c29cc643f3f4858f2cb4f9cf1e51f
olefile <mamorareolefile@gmail.com>
2026-03-23 15:54:37 +0200

    Adding a few tweaks

--- AI_Course/ai_chatbot/src/openai_client_response.py
+++ AI_Course/ai_chatbot/src/openai_client_response.py
@@ -2,9 +2,11 @@ from dotenv import load_dotenv, find_dotenv
 from openai import OpenAI
 import os
 
+
 load_dotenv(find_dotenv(), override=True)
 
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+#print(os.environ.get("OPENAI_API_KEY"))
 response = client.responses.create(
     model = 'gpt-4o',
     input="You are a Sci-Fi writer and poet. Write a poem about a lonely robot exploring a futuristic city.",
